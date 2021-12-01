36057bbaafdce0f3fd50c2f2978e615984f173af
Gremious Grenald <gremious@protonmail.com>
2021-12-01 23:56:40 +0000

    Part 1 feat folder structure

---
+++ src/day1/mod.rs
@@ -0,0 +1,5 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub mod part1;
+pub mod part2;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+mod day1;
+
+fn main() -> anyhow::Result<()> {
+    println!("{}", day1::part1::run()?);
+    Ok(())
 }
