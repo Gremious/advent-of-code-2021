c1619d7618eb8973bddcdef60c5d6e0ff1350f81
Gremious Grenald <gremious@protonmail.com>
2021-12-02 00:16:04 +0000

    Compare ints not strings lmao i'm tired

--- src/day1/part1.rs
+++ src/day1/part1.rs
@@ -2,9 +2,10 @@ use super::*;
 
 pub fn run() -> anyhow::Result<u64> {
     let file = File::open("./src/resources/1")?;
-    let lines = BufReader::new(file).lines().filter_map(|x| x.ok()).collect::<Vec<_>>();
+    let lines = BufReader::new(file).lines().map(|x| x.unwrap()).map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
 
-    let ret = lines.windows(2).fold(0, |acc, next| if next[1] > next[0] { acc + 1 } else { acc } ) + 1; // i actually dk why it's off by 1 lol
+    let ret = lines
+        .windows(2).fold(0, |acc, next| if next[1] > next[0] { acc + 1 } else { acc } ); // i actually dk why it's off by 1 lol
 
     Ok(ret)
 }
