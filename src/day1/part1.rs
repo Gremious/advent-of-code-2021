fdc4d133d93be6be310771e988564e7cc9adae54
Gremious Grenald <gremious@protonmail.com>
2021-12-02 00:17:43 +0000

    Remove dumb comment

--- src/day1/part1.rs
+++ src/day1/part1.rs
@@ -5,7 +5,7 @@ pub fn run() -> anyhow::Result<u64> {
     let lines = BufReader::new(file).lines().map(|x| x.unwrap()).map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
 
     let ret = lines
-        .windows(2).fold(0, |acc, next| if next[1] > next[0] { acc + 1 } else { acc } ); // i actually dk why it's off by 1 lol
+        .windows(2).fold(0, |acc, next| if next[1] > next[0] { acc + 1 } else { acc } );
 
     Ok(ret)
 }
