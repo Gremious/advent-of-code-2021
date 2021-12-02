06d6dae71bef22ca0f51f42c48e2e1d4c8869b90
Gremious Grenald <gremious@protonmail.com>
2021-12-02 00:51:26 +0000

    Part 2

--- src/day1/part2.rs
+++ src/day1/part2.rs
@@ -1,1 +1,16 @@
 use super::*;
+
+pub fn run() -> anyhow::Result<u64> {
+    let file = File::open("./src/resources/1")?;
+    let lines = BufReader::new(file).lines()
+        .map(|x| x.unwrap())
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+
+    // could use https://docs.rs/itertools/latest/itertools/trait.Itertools.html#method.chunks too if u wanted to skip a collect? idk how efficient it is
+    let sums = lines.windows(3).map(|x| x.into_iter().sum()).collect::<Vec<i32>>();
+    let ret = sums.windows(2).fold(0, |acc, next| if next[1] > next[0] { acc + 1 } else { acc } );
+
+    Ok(ret)
+}
+
