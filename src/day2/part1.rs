265ede4ded015645042773bd08d6bf2708ad9901
Gremious Grenald <gremious@protonmail.com>
2021-12-03 19:45:04 +0000

    Part 1

--- src/day2/part1.rs
+++ src/day2/part1.rs
@@ -1,5 +1,24 @@
 use super::*;
 
 pub fn run() -> anyhow::Result<u64> {
-    Ok(0)
+    let mut horizontal_position = 0;
+    let mut depth = 0;
+
+    let lines = BufReader::new(File::open("./src/resources/2")?).lines()
+        .map(|x| x.unwrap())
+        .collect::<Vec<String>>();
+
+    for line in lines {
+        let (command, value) = line.split_once(' ').unwrap();
+        let value = value.parse::<u64>()?;
+
+        match command {
+            "forward" => horizontal_position += value,
+            "down" => depth += value,
+            "up" => depth -= value,
+            _ => unreachable!(),
+        }
+    }
+
+    Ok(horizontal_position * depth)
 }
